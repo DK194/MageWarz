8af93ae73b25128e8cb21355b03b2ea7df836067
DK194 <decembermoon094@gmail.com>
2021-07-27 18:47:41 +0200

    Ready for deployment

---
+++ scripts/instruction.js
@@ -0,0 +1,158 @@
+const instructionIcons = document.querySelectorAll('.icon-info');
+const instructionModal = document.getElementById('instruction-modal');
+const iconInfo = document.getElementById('element-info');
+
+instructionIcons.forEach(icon => icon.addEventListener('click', showIconInfo));
+window.addEventListener('click', clearModal);
+
+function showIconInfo(e) {
+  const playerIcon = e.target.id;
+  
+  generateIconInfo(playerIcon);
+}
+
+function generateIconInfo(pI) {
+  let icon = generateIcon(pI);
+  let strengths = generateStrengths(pI);
+  let weaknesses = generateWeaknesses(pI);
+  let normalDmg = generateNormalDmg(pI);
+
+  iconInfo.innerHTML = `
+    ${icon}
+    <p>${pI.charAt(0).toUpperCase() + pI.slice(1)}</p>
+    <p>Strong against:</p>
+    <div class='icons-wrapper'>
+      ${strengths}
+    </div>
+    <p>Weak against:</p>
+    <div class='icons-wrapper'>
+      ${weaknesses}
+    </div>
+    <p>Damaged normally by:</p>
+      ${normalDmg}
+  `; 
+  
+  instructionModal.style.display = 'block';
+}
+
+function generateIcon(playerIcon) {
+  switch (playerIcon) {
+    case 'fire':
+      return `<i id='fire' class='main-icon fas fa-fire-alt fa-4x'></i>`;
+    case 'water':
+      return `<i id='water' class='main-icon fas fa-water fa-4x'></i>`;
+    case 'thunder':
+      return `<i id='thunder' class='main-icon fas fa-bolt fa-4x'></i>`;
+    case 'ice':
+      return `<i id='ice' class='main-icon far fa-snowflake fa-4x'></i>`;
+    case 'wind':
+      return `<i id='wind' class='main-icon fas fa-wind fa-4x'></i>`;
+    case 'earth':
+      return `<i id='earth' class='main-icon fas fa-mountain fa-4x'></i>`;
+  }
+}
+
+function generateStrengths(iconStrengths) {
+  switch (iconStrengths) {
+    case 'fire':
+      return `
+        <i id='ice' class='icon-info far fa-snowflake fa-3x'></i>
+        <i id='wind' class='icon-info fas fa-wind fa-3x'></i>
+      `;
+    case 'water':
+      return `
+        <i id='fire' class='icon-info fas fa-fire-alt fa-3x'></i>
+        <i id='earth' class='icon-info fas fa-mountain fa-3x'></i>
+      `;
+    case 'thunder':
+      return `
+        <i id='water' class='icon-info fas fa-water fa-3x'></i>
+        <i id='wind' class='icon-info fas fa-wind fa-3x'></i>
+      `;
+    case 'ice':
+      return `
+        <i id='water' class='icon-info fas fa-water fa-3x'></i>
+        <i id='thunder' class='icon-info fas fa-bolt fa-3x'></i>
+      `;
+    case 'wind':
+      return `
+        <i id='earth' class='icon-info fas fa-mountain fa-3x'></i>
+        <i id='ice' class='icon-info far fa-snowflake fa-3x'></i>
+      `;
+    case 'earth':
+      return `
+        <i id='thunder' class='icon-info fas fa-bolt fa-3x'></i>
+        <i id='fire' class='icon-info fas fa-fire-alt fa-3x'></i>
+      `;
+  }
+}
+
+function generateWeaknesses(iconWeaknesses) {
+  switch (iconWeaknesses) {
+    case 'fire':
+      return `
+        <i id='water' class='icon-info fas fa-water fa-3x'></i>
+        <i id='earth' class='icon-info fas fa-mountain fa-3x'></i>
+      `;
+    case 'water':
+      return `
+        <i id='ice' class='icon-info far fa-snowflake fa-3x'></i>
+        <i id='thunder' class='icon-info fas fa-bolt fa-3x'></i>
+      `;
+    case 'thunder':
+      return `
+        <i id='ice' class='icon-info far fa-snowflake fa-3x'></i>
+        <i id='earth' class='icon-info fas fa-mountain fa-3x'></i>
+      `;
+    case 'ice':
+      return `
+        <i id='wind' class='icon-info fas fa-wind fa-3x'></i>
+        <i id='fire' class='icon-info fas fa-fire-alt fa-3x'></i>
+      `;
+    case 'wind':
+      return `
+        <i id='thunder' class='icon-info fas fa-bolt fa-3x'></i>
+        <i id='fire' class='icon-info fas fa-fire-alt fa-3x'></i>
+      `;
+    case 'earth':
+      return `
+        <i id='water' class='icon-info fas fa-water fa-3x'></i>
+        <i id='wind' class='icon-info fas fa-wind fa-3x'></i>
+      `;
+  }
+}
+
+function generateNormalDmg(iconNormalDmg) {
+  switch (iconNormalDmg) {
+    case 'fire':
+      return `
+        <i id='thunder' class='icon-info fas fa-bolt fa-3x'></i>
+      `;
+    case 'water':
+      return `
+        <i id='wind' class='icon-info fas fa-wind fa-3x'></i>
+      `;
+    case 'thunder':
+      return `
+        <i id='fire' class='icon-info fas fa-fire-alt fa-3x'></i>
+      `;
+    case 'ice':
+      return `
+        <i id='earth' class='icon-info fas fa-mountain fa-3x'></i>
+      `;
+    case 'wind':
+      return `
+        <i id='water' class='icon-info fas fa-water fa-3x'></i>
+      `;
+    case 'earth':
+      return `
+        <i id='ice' class='icon-info far fa-snowflake fa-3x'></i>
+      `;
+  }
+}
+
+function clearModal(e) {
+  if (e.target === instructionModal) {
+    instructionModal.style.display = 'none';
+  }
+}
---
+++ scripts/main.js
@@ -0,0 +1,336 @@
+const highscore = document.getElementById('highscore');
+const playerHealthBar = document.getElementById('player-hp-bar');
+const playerHealthPoints = document.getElementById('player-health-points');
+const computerHealthBar = document.getElementById('computer-hp-bar');
+const computerHealthPoints = document.getElementById('computer-health-points');
+const restartButton = document.getElementById('restart-btn');
+const choices = document.querySelectorAll('.player-choice');
+const modal = document.querySelector('.modal');
+const result = document.getElementById('result');
+const initialGameData = {
+  playerScore: 0,
+  playerHP: 100,
+  computerHP: 100
+};
+
+choices.forEach(choice => choice.addEventListener('click', playGame));
+window.addEventListener('click', clearModal);
+restartButton.addEventListener('click', restartGame);
+
+function playGame(e) {
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const turnResult = getTurnResult(playerChoice, computerChoice);
+
+  showTurnResult(turnResult, computerChoice);
+
+  if (initialGameData.playerHP <= 0 || initialGameData.computerHP <= 0) {
+    showRoundWinner(computerChoice);
+  }
+
+  restartButton.style.display = 'inline-block';
+}
+
+function getComputerChoice() {
+  const rand = Math.floor(Math.random() * 6 + 1);
+
+  switch (rand) {
+    case 1:
+      return 'fire';
+    case 2:
+      return 'water';
+    case 3:
+      return 'thunder';
+    case 4:
+      return 'ice';
+    case 5:
+      return 'wind';
+    case 6:
+      return 'earth';
+  }
+}
+
+function getTurnResult(p, c) {
+  switch (p) {
+    case 'fire':
+      switch (c) {
+        case 'water':
+        case 'earth':
+          return 'Computer';
+        case 'ice':
+        case 'wind':
+          return 'Player';
+        case 'thunder':
+          return 'Costly Draw';
+        case 'fire':
+          return 'Draw';
+      }
+    case 'water':
+      switch (c) {
+        case 'ice':
+        case 'thunder':
+          return 'Computer';
+        case 'earth':
+        case 'fire':
+          return 'Player';
+        case 'wind':
+          return 'Costly Draw';
+        case 'water':
+          return 'Draw';
+      }
+    case 'thunder':
+      switch (c) {
+        case 'earth':
+        case 'ice':
+          return 'Computer';
+        case 'water':
+        case 'wind':
+          return 'Player';
+        case 'fire':
+          return 'Costly Draw';
+        case 'thunder':
+          return 'Draw';
+      }
+    case 'ice':
+      switch (c) {
+        case 'wind':
+        case 'fire':
+          return 'Computer';
+        case 'water':
+        case 'thunder':
+          return 'Player';
+        case 'earth':
+          return 'Costly Draw';
+        case 'ice':
+          return 'Draw';
+      }
+    case 'wind':
+      switch (c) {
+        case 'thunder':
+        case 'fire':
+          return 'Computer';
+        case 'earth':
+        case 'ice':
+          return 'Player';
+        case 'water':
+          return 'Costly Draw';
+        case 'wind':
+          return 'Draw';
+      }
+    case 'earth':
+      switch (c) {
+        case 'wind':
+        case 'water':
+          return 'Computer';
+        case 'thunder':
+        case 'fire':
+          return 'Player';
+        case 'ice':
+          return 'Costly Draw';
+        case 'earth':
+          return 'Draw';
+      }
+  }
+}
+
+function showTurnResult(trnRes, c) {
+  switch (trnRes) {
+    case 'Player':
+      calculateDamage('Player');
+      generateModal(trnRes, c);
+      break;
+    case 'Computer':
+      calculateDamage('Computer');
+      generateModal(trnRes, c);
+      break;
+    case 'Costly Draw':
+      calculateDamage('Costly Draw');
+      generateModal(trnRes, c);
+      break;
+    case 'Draw':
+      generateModal(trnRes, c);
+      break;
+  }
+
+  modal.style.display = 'block';
+}
+
+function calculateDamage(res) {
+  switch (res) {
+    case 'Player':
+      computerHealthBar.value -= 40;
+      initialGameData.computerHP -= 40;
+      break;
+    case 'Computer':
+      playerHealthBar.value -= 40;
+      initialGameData.playerHP -= 40;
+      break;
+    case 'Costly Draw':
+      computerHealthBar.value -= 20;
+      initialGameData.computerHP -= 20;
+      playerHealthBar.value -= 20;
+      initialGameData.playerHP -= 20;
+      break;
+  }
+
+  if (initialGameData.playerHP < 0) {
+    initialGameData.playerHP = 0;
+  } else if (initialGameData.computerHP < 0) {
+    initialGameData.computerHP = 0;
+  }
+
+  playerHealthPoints.innerHTML = `
+    ${initialGameData.playerHP}/100
+  `;
+  computerHealthPoints.innerHTML = `
+    ${initialGameData.computerHP}/100
+  `;
+}
+
+function generateModal(trnRes, c) {
+  modalTurnResult = generateModalResult(trnRes);
+  modalIcon = generateModalIcon(c);
+
+  result.innerHTML = `
+    ${modalTurnResult}
+    ${modalIcon}
+    <p class='modal-comp-choice'>
+      Computer Chose <strong>${c.charAt(0).toUpperCase() + c.slice(1)}</strong>
+    </p>
+  `; 
+}
+
+function generateModalResult(modRes) {
+  switch (modRes) {
+    case 'Player':
+      return `<h1 class='text-effective'>It's super effective!</h1>`;
+    case 'Computer':
+      return `<h1 class='text-ineffective'>It's not very effective...</h1>`;
+    case 'Costly Draw':
+      return `<h1 class='text-costly-draw'>A costly draw!</h1>`;
+    case 'Draw':
+      return `<h1>It's a draw!</h1>`;
+  }
+}
+
+function generateModalIcon(compChoice) {
+  switch (compChoice) {
+    case 'fire':
+      return `<i class='modal-icon player-choice fas fa-fire-alt fa-7x'></i>`;
+    case 'water':
+      return `<i class='modal-icon player-choice fas fa-water fa-7x'></i>`;
+    case 'thunder':
+      return `<i class='modal-icon player-choice fas fa-bolt fa-7x'></i>`;
+    case 'ice':
+      return `<i class='modal-icon player-choice far fa-snowflake fa-7x'></i>`;
+    case 'wind':
+      return `<i class='modal-icon player-choice fas fa-wind fa-7x'></i>`;
+    case 'earth':
+      return `<i class='modal-icon player-choice fas fa-mountain fa-7x'></i>`;
+  }
+}
+
+function restartGame() {
+  initialGameData.playerScore = 0;
+  playerHealthBar.value = 100;
+  initialGameData.playerHP = 100;
+  computerHealthBar.value = 100;
+  initialGameData.computerHP = 100;
+  
+  playerHealthPoints.innerHTML = `
+    100/100
+  `;
+  computerHealthPoints.innerHTML = `
+    100/100
+  `;
+  highscore.innerHTML = `
+    <h2 id='highscore' class='highscore'>Score: 0</h2>
+  `;
+
+  restartButton.style.display = 'none';
+}
+
+function showRoundWinner(c) {
+  if (initialGameData.playerHP <= 0 && initialGameData.computerHP <= 0) {
+    modalRoundResult = generateModalRoundResult('Draw');
+    generateRoundModal(c, modalRoundResult);
+    nextRoundStart();
+  } else if (initialGameData.computerHP <= 0) {
+    initialGameData.playerScore += initialGameData.playerHP;
+    modalRoundResult = generateModalRoundResult('Win');
+    generateRoundModal(c, modalRoundResult);
+    nextRoundStart();
+  } else if (initialGameData.playerHP <= 0) {
+    modalRoundResult = generateModalRoundResult('Lose');
+    generateRoundModal(c, modalRoundResult);
+    nextRoundStartLose();
+  } 
+}
+
+function generateModalRoundResult(rndRes) {
+  switch (rndRes) {
+    case 'Draw':
+      return `<h1>Round resulted in a draw.</h1>`;
+    case 'Win':
+      return `<h1 class='text-win'>You won the round!</h1>`;
+    case 'Lose':
+      return `<h1 class='text-lose'>You lost the round.</h1>`;
+  }
+}
+
+function generateRoundModal(compChoice, mRndRes) {
+  modalIcon = generateModalIcon(compChoice);
+  rndScore = initialGameData.playerScore;
+
+  result.innerHTML = `
+    ${mRndRes}
+    <p class='modal-score'>Your score: ${rndScore}</p>
+    ${modalIcon}
+    <p class='modal-comp-choice'>
+      Computer Chose <strong>${compChoice.charAt(0).toUpperCase() + compChoice.slice(1)}</strong>
+    </p>
+  `;
+}
+
+function nextRoundStart() {
+  playerHealthBar.value = 100;
+  initialGameData.playerHP = 100;
+  computerHealthBar.value = 100;
+  initialGameData.computerHP = 100;
+
+  let score = initialGameData.playerScore;
+
+  playerHealthPoints.innerHTML = `
+    100/100
+  `;
+  computerHealthPoints.innerHTML = `
+    100/100
+  `;
+  highscore.innerHTML = `
+    <h2 id='highscore' class='highscore'>Score: ${score}</h2>
+  `;
+}
+
+function nextRoundStartLose() {
+  initialGameData.playerScore = 0;
+  playerHealthBar.value = 100;
+  initialGameData.playerHP = 100;
+  computerHealthBar.value = 100;
+  initialGameData.computerHP = 100;
+  
+  playerHealthPoints.innerHTML = `
+    100/100
+  `;
+  computerHealthPoints.innerHTML = `
+    100/100
+  `;
+  highscore.innerHTML = `
+    <h2 id='highscore' class='highscore'>Score: 0</h2>
+  `;
+}
+
+function clearModal(e) {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+}
